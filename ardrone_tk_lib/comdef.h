33ee54da54770fb75ae7990223db337d116c2359
tkorays <tkorays@hotmail.com>
2014-05-30 09:31:00 +0800

    mod

--- ardrone_tk_lib/comdef.h
+++ ardrone_tk_lib/comdef.h
@@ -3,9 +3,17 @@
 #pragma once
 #define LF_DEF '\12'
 #define CR_DEF '\12'
-#define NAVDATA_PORT 5554
-#define AT_COMMAND_PORT 5556
 
+
+#define FTP_PORT	         5551
+#define AUTH_PORT            5552
+#define VIDEO_RECORDER_PORT  5553
+#define NAVDATA_PORT         5554
+#define VIDEO_PORT           5555
+#define AT_PORT              5556
+#define RAW_CAPTURE_PORT     5557
+#define PRINTF_PORT          5558
+#define CONTROL_PORT         5559
 
 
 #endif // _TK_COMDEF_H_
