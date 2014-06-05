4f159ec1caee76a8b25c72891497268dc6e56154
tkorays <tkorays@hotmail.com>
2014-06-05 16:39:38 +0800

    demo

--- v_1.0.0_demo/nav_test.cpp
+++ v_1.0.0_demo/nav_test.cpp
@@ -14,66 +14,45 @@ Mutex cmd_id_mutex;
 __CLAIM_THREAD_TABLE__
 
 void callback(void* param) {
-	data_pack* pack = (data_pack*)param;
+	// 从参数中获取数据
+	navdata_t* navdata = (navdata_t*)((data_pack*)param)->data;
 	cout << endl << "received..." << endl << endl;
+	// 显示数据
+	printf("header:   0x%x\n", navdata->header);
+	printf("state:    0x%x\n", navdata->ardrone_state);
+	printf("seq:      0x%x\n", navdata->sequence);
+	printf("vision:   0x%x\n", navdata->vision_defined);
 }
 
 int main(int argc, char** argv) {
-	// state检验
-	uint32_t state = 0x4f800c90;
-	ardrone_get_mask(state, ARDRONE_FLY_MASK) ? \
-		cout << "fly" << endl:cout << "not fly" << endl;
-	ardrone_get_mask(state, ARDRONE_VIDEO_MASK) ? \
-		cout << "video on" << endl:cout << "video off" << endl;
-	ardrone_get_mask(state, ARDRONE_VISION_MASK) ? \
-		cout << "vision on" << endl:cout << "vision off" << endl;
-	ardrone_get_mask(state, ARDRONE_NAVDATA_BOOTSTRAP) ? \
-		cout << "is bootstrap" << endl:cout << "not bootstrap" << endl;
-	ardrone_get_mask(state, ARDRONE_NAVDATA_DEMO_MASK) ? \
-		cout << "demo" << endl:cout << "not demo" << endl;
-
-
 	net_prepare();
-
 	UdpClient at_client(ARDRONE_IP, AT_PORT);
 	UdpClient nav_client(ARDRONE_IP, NAVDATA_PORT);
 	string cmd = "\1";
 	
-	navdata_t navdata;
+	navdata_t navdata; // 用于保存navdata
 	nav_client.send(cmd.c_str(), 1); // 发送数据以获得状态信息
-	//gen.cmd_land();
+	// 可以在回调函数里面使用一些功能
+	bool recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
+	if (recved) {
+		// 如果是bootstrap模式就退出bootstrap模式
+		if (ardrone_get_mask(navdata.ardrone_state,ARDRONE_NAVDATA_BOOTSTRAP)) {
+			cmd = gen.cmd_config("general:navdata_demo", "TRUE");
+			at_client.send(cmd.c_str(), cmd.size());
+		}
+	}
+		
+	recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
+
+	// 发送一个ctrl命令，接收连续的数据
+	cmd = gen.cmd_control(5, 0);
+	at_client.send(cmd.c_str(), cmd.size());
+
+	// 这里可以传入一个可以处理option数据的callback函数
+	while (true) {
+		recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
+	}
 	
-	//while (true) {
-
-		bool recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
-		if (recved) {
-			printf("header:   0x%x\n", navdata.header);
-			printf("state:    0x%x\n", navdata.ardrone_state);
-			printf("seq:      0x%x\n", navdata.sequence);
-			printf("vision:   0x%x\n", navdata.vision_defined);
-			
-		}
-		cmd = gen.cmd_config("general:navdata_demo", "TRUE");
-		at_client.send(cmd.c_str(), cmd.size());
-		recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
-		if (recved) {
-			printf("header:   0x%x\n", navdata.header);
-			printf("state:    0x%x\n", navdata.ardrone_state);
-			printf("seq:      0x%x\n", navdata.sequence);
-			printf("vision:   0x%x\n", navdata.vision_defined);
-
-		}
-		cmd = gen.cmd_control(5, 0);
-		at_client.send(cmd.c_str(), cmd.size());
-		recved = nav_client.recv((char*)&navdata, sizeof(navdata), callback);
-		if (recved) {
-			printf("header:   0x%x\n", navdata.header);
-			printf("state:    0x%x\n", navdata.ardrone_state);
-			printf("seq:      0x%x\n", navdata.sequence);
-			printf("vision:   0x%x\n", navdata.vision_defined);
-
-		}
-
 	//}
 	
 	net_end();
