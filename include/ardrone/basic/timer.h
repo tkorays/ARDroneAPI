ed5f208d65e4023482bca215f3f05191d3c2692b
tkorays <tkorays@hotmail.com>
2014-06-06 15:32:50 +0800

    时钟的定义

--- include/ardrone/basic/timer.h
+++ include/ardrone/basic/timer.h
@@ -1,6 +1,20 @@
 #ifndef _TK_TIMER_H_
 #define _TK_TIMER_H_
+#include "type.h"
 
-
+namespace whu {
+	class Timer {
+	private:
+		class Impl;
+		Impl* impl;
+		uint32_t interval;
+		bool valid;
+	public:
+		Timer();
+		Timer(uint32_t interval_, void(*call_fun)(void*param),bool auto_start = true);
+		bool start();
+		static void sleep(uint32_t sleep_time);
+	};
+}
 
 #endif // _TK_TIMER_H_
