74544fda66c826ede4dfd0a3d32a03f626181b24
tkorays <tkorays@hotmail.com>
2014-06-08 18:56:31 +0800

    navdata数据获取demo

--- test_src/navdatademo.cpp
+++ test_src/navdatademo.cpp
@@ -10,6 +10,7 @@
 #include <ardrone/basic/kbpress.h>
 #include <ardrone/basic/timer.h>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 using namespace whu;
 using namespace whu::ardrone;
@@ -17,27 +18,44 @@ using namespace whu::ardrone;
 #pragma comment(lib,"ARDrone.lib")
 
 UdpClient* recvClient;
+void check_option(void* p) {
+	// 前期设计问题...
+
+	const navdata_option_t* option = ((navdata_t*)((data_pack*)p)->data)->options;
+	cout << "---------------------" << endl;
+	if (option->tag==navdata_tag_t::NAVDATA_CKS_TAG) {
+		cout << "这是一个校验和。。。" << endl;
+	}
+	cout << "---------------------" << endl;
+}
 
 void data_recv_func(void* param) {
 	// 这里面接收数据
 	char data[2048];
-	recvClient->recv(data, 2048, nullptr); // 回调函数传入的参数为data
+	recvClient->recv(data, 2048, check_option); // 回调函数传入的参数为data
 	const navdata_t* nvdt = (navdata_t*)data;
-	system("cls");
-	cout << " header: " << nvdt->header << endl;
-	cout << " vision: " << nvdt->vision_defined << endl;
 	
+	cout << "消息头: 0x" << hex <<setw(8) << setfill('0') << nvdt->header << endl;
+	cout << "vision: " << nvdt->vision_defined << endl;
+	cout << "编号: 0x" << hex << setw(8) << setfill('0') << nvdt->sequence << endl;
+	cout << "飞行状态:" << (ardrone_get_mask(nvdt->ardrone_state, ARDRONE_FLY_MASK) ? "飞行中" : "不在飞行")<<endl;
+	cout << "模式： " << (ardrone_get_mask(nvdt->ardrone_state, ARDRONE_NAVDATA_BOOTSTRAP) ? "bootstrap模式" : "已经推出bootstrap模式") << endl;
+	cout << endl;
 }
 
 int main(int argc,char** argv) {
 	net_prepare();
+	
 	recvClient = new UdpClient(ARDRONE_IP, NAVDATA_PORT);
+	
 	if (!recvClient->is_valid()) {
 		cout << "udp client invalid..." << endl;
 		return 0;
 	}
+	// 接收之前貌似必须得发送一个包到服务器。
+	recvClient->send("\1", 1);
 	cout << "Recieve data every 50ms.." << endl;
-	Timer timer(50, data_recv_func, nullptr, false);
+	Timer timer(100, data_recv_func, nullptr, false);
 	timer.start();
 
 	net_end();
