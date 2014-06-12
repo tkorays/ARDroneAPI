4a8836215e2c86175bcfd45dc70a308bf67c4045
tkorays <tkorays@hotmail.com>
2014-06-12 14:32:38 +0800

    demo

--- test_src/fly_recv.cpp
+++ test_src/fly_recv.cpp
@@ -9,7 +9,14 @@ ARDroneClient* arClient;
 int main(int argc,char** argv){
 	arClient = new ARDroneClient();
 	arClient->InitARDrone();
+	// 发送看门狗
 	arClient->SendATCmd(arClient->atgen.cmd_watchdog());
+	// 起飞
+	arClient->SendATCmd(arClient->atgen.cmd_takeoff());
+	// 控制飞行
+	arClient->SendATCmd(arClient->atgen.cmd_move(true, -0.8f, 0.0f, 0.0f,0.0f));
+	// 降落
+	arClient->SendATCmd(arClient->atgen.cmd_land());
 	delete arClient;
 	return 0;
 }
