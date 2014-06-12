3f634ffbf62b9001a22be4f9886760241f8ff377
tkorays <tkorays@hotmail.com>
2014-06-12 17:58:28 +0800

    参数修改:

--- test_src/fly_recv.cpp
+++ test_src/fly_recv.cpp
@@ -11,22 +11,25 @@ bool kb_control(void*) {
 	if (key_press(KEY_SPACE)) {
 		if (is_onland) {
 			arClient->SendATCmd(arClient->atgen.cmd_takeoff());
+			Timer::sleep(2000);
 			is_onland = false;
-		} else {		arClient->SendATCmd(arClient->atgen.cmd_land());
+		} else {		
+			arClient->SendATCmd(arClient->atgen.cmd_land());
+			Timer::sleep(2000);
 			is_onland = true;
 		}
 	}
 	if (key_press(KEY_LEFT)) {
-		arClient->SendATCmd(arClient->atgen.cmd_move(true, -0.3f, 0.0f, 0.0f, 0.0f));
+		arClient->SendATCmd(arClient->atgen.cmd_move(true, -0.2f, 0.0f, 0.0f, 0.0f));
 	}
 	if (key_press(KEY_RIGHT)) {
-		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.3f, 0.0f, 0.0f, 0.0f));
+		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.2f, 0.0f, 0.0f, 0.0f));
 	}
 	if (key_press(KEY_UP)) {
-		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.0f, -0.3f, 0.0f, 0.0f));
+		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.0f, -0.2f, 0.0f, 0.0f));
 	}
 	if (key_press(KEY_DOWN)) {
-		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.0f, 0.3f, 0.0f, 0.0f));
+		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.0f, 0.2f, 0.0f, 0.0f));
 	}
 	if (key_press(KEY_RETURN)) {
 		return false;
