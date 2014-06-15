008c925a5eddbf7c5efdfdeaf3c090ec1cc05ddc
tkorays <tkorays@hotmail.com>
2014-06-15 14:30:38 +0800

    demo，接收机数据&&控制

--- test_src/fly_recv.cpp
+++ test_src/fly_recv.cpp
@@ -1,9 +1,15 @@
 #include <ardrone/ardrone.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 using namespace whu;
 using namespace whu::ardrone;
 #pragma comment(lib,"ARDrone.lib")
 
+HANDLE recv_thread;
+HANDLE wd_thread;
+DWORD tid;
 ARDroneClient* arClient;
 bool is_onland = true;
 
@@ -32,18 +38,54 @@ bool kb_control(void*) {
 		arClient->SendATCmd(arClient->atgen.cmd_move(true, 0.0f, 0.2f, 0.0f, 0.0f));
 	}
 	if (key_press(KEY_RETURN)) {
+		CloseHandle(recv_thread);
+		CloseHandle(wd_thread);
 		return false;
 	}
 	return true;
 }
+// 真正的处理函数,这里只读取了高度等信息
+void handle_recv_data(void* dt_pck) {
+	data_pack* dt = (data_pack*)dt_pck;
+	NavdataParser parser(dt->data,dt->len);
+	
+	if (!parser.state_bootstrap()) { 
+		// 如果已经退出bootstrap模式，则输出相关的信息
+		parser.read_option_list();
+		const navdata_demo_t* demo = ((navdata_demo_t*)((navdata_t*)dt->data)->options);
+		printf("\n----------------------------------------------\n");
+		printf("高度:\t%d\n", parser.idata.altitude);
+		printf("电量:\t%d%%\n", parser.idata.voltage);
+		printf("vx:\t%f\n", parser.idata.vx);
+		printf("vy:\t%f\n", parser.idata.vy);
+		printf("vz:\t%f\n", parser.idata.vz);
+	}
+}
+// 接收机线程，不处理
 DWORD WINAPI recv_data(PVOID param) {
-
+	while (true) {
+		arClient->RecvData(handle_recv_data);
+		Timer::sleep(30);
+	}
 	return 0;
 }
-
+// 不是看门狗，数据发了一段时间如果不回复，他会停止发送，所以隔一段时间就要发送一次通知
+DWORD WINAPI watch_dog(LPVOID param) {
+	while (true) {
+		arClient->navClient->send("\1", 1);
+		Timer::sleep(2000);
+	}
+	return 0;
+}
 int main(int argc,char** argv){
 	arClient = new ARDroneClient();
 	arClient->InitARDrone();
+	// 开启线程接收数据
+	
+	recv_thread = CreateThread(NULL, 0, recv_data, NULL, 0, &tid);
+	DWORD wd_tid;
+	wd_thread = CreateThread(NULL, 0, watch_dog, NULL, 0, &wd_tid);
+
 	// 发送看门狗
 	arClient->SendATCmd(arClient->atgen.cmd_watchdog());
 	// flat trims
@@ -55,6 +97,7 @@ int main(int argc,char** argv){
 	Timer timer_ctrl(50,kb_control,nullptr,false);
 	timer_ctrl.start();
 
+	
 	delete arClient;
 	return 0;
 }
